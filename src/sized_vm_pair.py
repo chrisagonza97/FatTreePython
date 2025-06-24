e2ac35e50077d0d19bf72059a0c3eddb71998542
Chris <chris.a.gonzalez97@gmail.com>
2025-06-24 16:06:37 -0700

    added import to sized vmpair class

--- src/sized_vm_pair.py
+++ src/sized_vm_pair.py
@@ -1,3 +1,5 @@
+from .vm_pair import VmPair
+
 class SizedVmPair(VmPair):
     def __init__(self, first_vm_location, second_vm_location, traffic_rate, vm_size):
         super().__init__(first_vm_location, second_vm_location, traffic_rate)
